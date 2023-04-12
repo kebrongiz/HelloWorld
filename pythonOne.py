49597a53899370e0c1bb9ba9b278d1b5cb01e8fd
kebron EL <kmamegiza@gmail.com>
2023-04-13 01:05:12 +0300

    #Getting Started with Python

---
+++ pythonOne.py
@@ -0,0 +1,44 @@
+print ("Hello world")
+print ("   /|")
+print ("  / |")
+print (" /  |")
+print ("/___|")
+print("kebron \" Gizachew \"")
+name_check = "eli" ,"abel"
+print(name_check)
+name="Kebron Gizachew Asrat"
+print(name.lower() + " is cool")
+print("is cool " + name)
+print(name.upper().isupper())
+print(len(name))
+print(name[3])
+print(name_check[1])
+print(name.index("K"))
+print(name.replace("Gizachew","ABABA"))
+print(2)
+print(-3.86)
+print((5+5)*4) # BODMAS
+print(10 % 3)
+my_num = 7
+print( str(my_num) + " is my favorite number.")
+print("my favorite number " + str(my_num)) # see datatype changing
+print(str(my_num))
+num=-9
+print(abs(num))
+print(max(4, 5))
+print(pow(4, 5))
+print(round(6.784))
+name = input("Enter your name:")           # inputting data from the user
+print("Hello " + name)
+
+num1 = input("Enter the first number:")
+num2 = input("Enter the second number:")
+result = int(num1) + float(num2)           # changing data type
+print(result)
+friends = ["kebron","Gizachew","Asrat"]
+friends[2]="Mike"
+print(friends)
+print(friends[-3])
+lucky_numbers = [3, 5, 6, 7]
+print(friends[2])
+print(lucky_numbers[1:2])
